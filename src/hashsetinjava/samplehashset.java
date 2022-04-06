a35ea70305f3975e9bacfc0868166c87a40e2b3a
Sudheer Junnuthula <gantar.swathi@gmail.com>
2022-04-05 21:10:41 -0500

    updated setinjava

---
+++ src/hashsetinjava/samplehashset.java
@@ -0,0 +1,47 @@
+package hashsetinjava;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class samplehashset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet hs=new HashSet<>();
+		hs.add(30);
+		hs.add(40);
+		hs.add('v');
+		hs.add("Hash");
+		hs.add(null);
+		System.out.println("hs = "+hs);
+		
+		
+		LinkedList<String> l2=new LinkedList();
+		l2.add("tom");
+		l2.add("tonny");
+		l2.add("Link");
+		l2.add("tri");
+		l2.add(null);
+		l2.add("trip");
+		System.out.println("l2 = "+l2);
+
+		ArrayList<String> al2= new ArrayList<String> ();
+		al2.add("tom");
+		al2.add("amit");
+		al2.add("sumit");
+		al2.add("smart");
+		al2.add("home");
+		System.out.println("al2 = "+al2);
+	//	System.out.println(hs);
+		//System.out.println(hs.size());
+		System.out.println(hs.addAll(al2));
+		System.out.println(hs);
+		hs.contains(al2);
+		for(Object elements:hs) {
+			System.out.println(elements);
+			System.out.println(hs);
+		}
+	}
+
+}
---
+++ src/treeset/treesetinjava.java
@@ -0,0 +1,39 @@
+package treeset;
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class treesetinjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet ts= new TreeSet<>();
+		ts.add(20);
+		ts.add(20);
+		ts.add(30);
+		ts.add(50);//here treeSet is not heterogeneous 
+					//TreeSet is homogeneous and no duplicates
+		System.out.println(ts);
+		TreeSet ts1=new TreeSet <>();
+		ts1.add("Swathi");
+		ts1.add("Asha");
+		ts1.add("Sravnathi");
+		ts1.add("Yamini");
+		
+		HashSet hs=new HashSet<>();
+		hs.add(30);
+		hs.add(40);
+		hs.add('v');
+		hs.add("Asha");
+		hs.add(null);
+		
+		System.out.println(ts1);
+		System.out.println(ts1.first());
+	System.out.println(ts.retainAll(hs));//it will print true or false and  compare and remove all different elements from treeset
+	System.out.println(ts.pollFirst());//print and remove first element
+//ystem.out.println(ts.);
+	//		for(Object elements:ts1) {
+//			System.out.println(elements);
+//		}
+	}
+
+}
